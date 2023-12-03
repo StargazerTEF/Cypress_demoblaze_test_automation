ce6fc98d715d16496b86c1d7156c064bf848a2c8
Marko Peric <markoperic986@gmail.com>
2023-12-03 01:03:41 +0100

    Add Login tests - Verify the login form title

--- cypress/e2e/login_tests.cy.js
+++ cypress/e2e/login_tests.cy.js
@@ -1,6 +1,13 @@
 const NavigationPage = require("../support/page-object/NavigationPage");
 const LoginPage = require("../support/page-object/LoginPage");
 describe('Sign up tests', () => {
+        let titles
+
+        before('Load alerts', () => {
+            cy.fixture('titles.json').then(data => {
+                titles = data
+            })
+        })
 
     beforeEach('Visit base url', () => {
         cy.visit(Cypress.config().baseUrl)
@@ -9,5 +16,10 @@ describe('Sign up tests', () => {
     it('Verify that login form is visible', () => {
         NavigationPage.clickLoginLink()
         LoginPage.verifyThatLoginFormIsVisible()
+    })
+
+    it('Verify the login form title', () => {
+        NavigationPage.clickLoginLink()
+        LoginPage.verifyLoginFormTitle(titles.loginFormTitle)
     })
 })
--- cypress/support/page-object/LoginPage.js
+++ cypress/support/page-object/LoginPage.js
@@ -1,11 +1,16 @@
 class LoginPage {
 
     elements = {
-        loginForm: () => cy.get('div#logInModal .modal-content')
+        loginForm: () => cy.get('div#logInModal .modal-content'),
+        loginFormTitle: () => cy.get('#logInModalLabel')
     }
 
     verifyThatLoginFormIsVisible() {
         this.elements.loginForm().should('be.visible')
+    }
+
+    verifyLoginFormTitle(title) {
+        this.elements.loginFormTitle().should('have.text', title)
     }
 }
 
