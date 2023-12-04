a79c845a2b9b5a25d43947da501560cc454849f5
Marko Peric <markoperic986@gmail.com>
2023-12-04 23:37:02 +0100

    Add Cart tests - Verify the names of added products

--- cypress/support/page-object/CartPage.js
+++ cypress/support/page-object/CartPage.js
@@ -1,13 +1,18 @@
 class CartPage {
 
     elements = {
-        addedProducts: () => cy.get('#tbodyid > .success')
+        addedProducts: () => cy.get('#tbodyid > .success'),
+        addedProductNames: ()=> cy.get('td:nth-child(2)')
     }
 
     verifyThatAddedProductsAreVisible() {
         this.elements.addedProducts().each(($el, index, $list)=>{
             this.elements.addedProducts().eq(index).should('be.visible')
         })
+    }
+
+    verifyTheNameOfAddedProduct(name) {
+            this.elements.addedProductNames().should('contain.text', name)
     }
 }
 
